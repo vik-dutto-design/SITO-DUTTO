38e700eaa542dc2e252b177e60b0b20becc52b11
vik-dutto-design <duttovittoria@gmail.com>
2026-03-10 11:07:26 +0100

    main.js

--- main.js
+++ main.js
@@ -195,46 +195,5 @@ function resetCarouselState() {
 /*************************************************    CAROSELLO    ***********************************************************/
 
 /*************************************************  PAGINA CONTATTI  ***********************************************************/
-// Input Animation
 
-const inputs = document.querySelectorAll('.input');
-
-function focusFunc() {
-    let parent = this.parentNode;
-    parent.classList.add('focus');
-}
-
-function blurFunc() {
-    let parent = this.parentNode;
-    if(this.value == "") {
-        parent.classList.remove('focus');
-    }
-}
-
-inputs.forEach((input) => {
-    input.addEventListener('focus', focusFunc);
-    input.addEventListener('blur', blurFunc);
-})
-
-//Toggle Themes
-
-const toggleButton = document.getElementById('theme-toggle');
-const themeOptions = document.querySelector('.theme-options');
-const radioButtons = document.querySelectorAll('input[name="theme"]');
-
-// Toggle dropdown visibility
-toggleButton.addEventListener('click', () => {
-  document.querySelector('.theme-switcher').classList.toggle('active');
-});
-
-// Apply theme on selection
-radioButtons.forEach(radio => {
-  radio.addEventListener('change', () => {
-    const selectedTheme = radio.value;
-    document.documentElement.className = selectedTheme;
-  });
-});
-
-// Set default theme (Dark Blue)
-document.documentElement.className = 'dark-blue';
 /*************************************************  PAGINA CONTATTI  ***********************************************************/
